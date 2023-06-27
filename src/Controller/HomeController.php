95bd5e24edd5561aa817bae2eedb40adba157d69
28010-08-08 <28010-08-08@exchange.ad.afpanet>
2023-06-27 16:44:38 +0200

    start-accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class InscriptionController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_inscription')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('inscription/index.html.twig', [
-            'controller_name' => 'InscriptionController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
---
+++ src/Entity/user.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Entity;
+
+class User
+{
+    private $id;
+    private $username;
+    private $email;
+    private $password;
+}
