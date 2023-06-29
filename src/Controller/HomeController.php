442e9593b5db47913c9bfcbdf40fd11285bf15ec
28010-08-08 <28010-08-08@exchange.ad.afpanet>
2023-06-29 11:56:28 +0200

    start_api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp\Client;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        $apiKey = 'ab38bfe090bf476b84e7454698452d66';
+        $url = 'https://api.rawg.io/api/games';
+
+        $client = new Client();
+        $response = $client->request('GET', $url, [
+            'query' => [
+                'key' => $apiKey,
+                //'genres' => 'indie',
+            ],'curl' => [
+                CURLOPT_SSL_VERIFYPEER => false,
+            ],
+        ]);
+        
+        $statusCode = $response->getStatusCode();
+        $content = $response->getBody()->getContents();
+        $games = json_decode($content, true);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'status_code' => $statusCode,
+            'games' => $games['results'],
         ]);
     }
 }
