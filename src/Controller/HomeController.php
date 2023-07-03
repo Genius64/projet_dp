82676d0a5cb6e271817ca2e07a11691c4084f806
28010-08-08 <28010-08-08@exchange.ad.afpanet>
2023-07-03 13:59:31 +0200

    start_form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,56 +14,56 @@ class HomeController extends AbstractController
     {
         $apiKey = 'ab38bfe090bf476b84e7454698452d66';
         $url = 'https://api.rawg.io/api/games';
-    $genresUrl = 'https://api.rawg.io/api/genres';
+        $genresUrl = 'https://api.rawg.io/api/genres';
 
-    $client = new Client();
+        $client = new Client();
 
-    // Récupérer les jeux les mieux notés
-    $topRatedResponse = $client->request('GET', $url, [
-        'query' => [
-            'key' => $apiKey,
-            'ordering' => '-rating', // Pour obtenir les jeux par ordre décroissant de note
-        ], 'curl' =>[
-            CURLOPT_SSL_VERIFYPEER => false,
-        ],
-    ]);
-    $topRatedContent = $topRatedResponse->getBody()->getContents();
-    $topRatedGames = json_decode($topRatedContent, true)['results'];
-
-    // Récupérer la liste des genres
-    $genresResponse = $client->request('GET', $genresUrl, [
-        'query' => [
-            'key' => $apiKey,
-        ], 'curl' =>[
-            CURLOPT_SSL_VERIFYPEER => false,
-        ],
-    ]);
-    $genresContent = $genresResponse->getBody()->getContents();
-    $genres = json_decode($genresContent, true)['results'];
-
-    // Récupérer les jeux par genre
-    $gamesByGenre = [];
-
-    foreach ($genres as $genre) {
-        $genreSlug = $genre['slug'];
-        $genreResponse = $client->request('GET', $url, [
+        // Récupérer les jeux les mieux notés
+        $topRatedResponse = $client->request('GET', $url, [
             'query' => [
                 'key' => $apiKey,
-                'genres' => $genreSlug,
+                'ordering' => '-rating', // Pour obtenir les jeux par ordre décroissant de note
             ], 'curl' =>[
                 CURLOPT_SSL_VERIFYPEER => false,
             ],
         ]);
-        $genreContent = $genreResponse->getBody()->getContents();
-        $genreGames = json_decode($genreContent, true)['results'];
-        $gamesByGenre[$genreSlug] = $genreGames;
-    }
+        $topRatedContent = $topRatedResponse->getBody()->getContents();
+        $topRatedGames = json_decode($topRatedContent, true)['results'];
 
-    return $this->render('home/index.html.twig', [
-        'controller_name' => 'HomeController',
-        'topRatedGames' => $topRatedGames,
-        'gamesByGenre' => $gamesByGenre,
-        'genres' => $genres,
-    ]);
+        // Récupérer la liste des genres
+        $genresResponse = $client->request('GET', $genresUrl, [
+            'query' => [
+                'key' => $apiKey,
+            ], 'curl' =>[
+                CURLOPT_SSL_VERIFYPEER => false,
+            ],
+        ]);
+        $genresContent = $genresResponse->getBody()->getContents();
+        $genres = json_decode($genresContent, true)['results'];
+
+        // Récupérer les jeux par genre
+        $gamesByGenre = [];
+
+        foreach ($genres as $genre) {
+            $genreSlug = $genre['slug'];
+            $genreResponse = $client->request('GET', $url, [
+                'query' => [
+                    'key' => $apiKey,
+                    'genres' => $genreSlug,
+                ], 'curl' =>[
+                    CURLOPT_SSL_VERIFYPEER => false,
+                ],
+            ]);
+            $genreContent = $genreResponse->getBody()->getContents();
+            $genreGames = json_decode($genreContent, true)['results'];
+            $gamesByGenre[$genreSlug] = $genreGames;
+        }
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'topRatedGames' => $topRatedGames,
+            'gamesByGenre' => $gamesByGenre,
+            'genres' => $genres,
+        ]);
     }
 }
